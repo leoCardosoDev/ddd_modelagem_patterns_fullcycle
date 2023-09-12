6fc6ddd55880efb946b341cc0256d408659443e3
Leo Silva <leocardosodev@gmail.com>
2023-09-12 15:30:19 -0300

    feat: entidades focadas em negocios

--- src/entities/customer.ts
+++ src/entities/customer.ts
@@ -4,17 +4,37 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
+  _active: boolean = false;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this.validate();
   }
 
-  get id(): string {return this._id};
-  get name(): string {return this._name};
-  get address(): string {return this._address};
+  validate(){
+    if (this._name.length === 0) { 
+      throw new Error("Name is required!");
+    }
+    if (this._id.length === 0) { 
+      throw new Error("ID is required!");
+    }
+  }
 
-  set name(name: string){this._name = name}
-  set address(address: string){this._address = address}
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate(){
+    if(this._address.length === 0) {
+      throw new Error("Address is mandatory to activate as customer");
+    }
+    this._active = true;
+  }
+  
+  deactivate(){
+    this._active = false;
+  }
 }
