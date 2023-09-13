dc5d43ecd61b8c6449871ec07986bebd4f712f60
Leo Silva <leocardosodev@gmail.com>
2023-09-13 12:08:09 -0300

    feat: adicionando objetos de valor

---
+++ src/values_objects/customer.ts
@@ -0,0 +1,44 @@
+import Address from "./address";
+
+class CustomerValueObject {
+  _id: string;
+  _name: string;
+  _address!: Address;
+  _active: boolean = false;
+
+  constructor(id: string, name: string, address: Address) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+    this.validate();
+  }
+
+  validate(){
+    if (this._name.length === 0) { 
+      throw new Error("Name is required!");
+    }
+    if (this._id.length === 0) { 
+      throw new Error("ID is required!");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate(){
+    if(this._address != undefined) {
+      throw new Error("Address is mandatory to activate as customer");
+    }
+    this._active = true;
+  }
+
+  set Address(address: Address) {
+    this._address = address;
+  }
+  
+  deactivate(){
+    this._active = false;
+  }
+}
